c57273473d506805aff659d95d65fa5108d35e7e
narimansaud-alt <narimansaud@gmail.com>
2026-06-26 23:39:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Подставляет хэш коммита в sw.js при деплое на Cloudflare Pages
 HASH=${CF_PAGES_COMMIT_SHA:-$(date +%s)}
-SHORT=${HASH:0:8}
-sed "s/__BUILD_HASH__/$SHORT/g" sw.js > sw.js.tmp && mv sw.js.tmp sw.js
+SHORT=$(echo "$HASH" | cut -c1-8)
+sed "s/__BUILD_HASH__/$SHORT/g" sw.js > sw_tmp.js && mv sw_tmp.js sw.js
 echo "Build hash: $SHORT"
